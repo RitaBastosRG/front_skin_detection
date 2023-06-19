4a083a75f1e8db8ea5ddcbfe1943bb39634b314a
Rita Bastos ritabastos02.dgup@gmail.com <ritabastos02@gmail.com>
2023-06-19 13:25:03 +0100

    Changes colours

--- front_end_skin/skin_cancer_detection.py
+++ front_end_skin/skin_cancer_detection.py
@@ -12,65 +12,47 @@ st.set_page_config(
     layout="centered", # wide
     initial_sidebar_state="collapsed") # collapsed)
 
-# add gradient background
-page_bg_img = '''
-<style>
-.stApp {
-  background-image: url("https://images.unsplash.com/photo-1621799754526-a0d52c49fad5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80");
-  background-size: cover;
-}
-</style>
-'''
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 ############# Config the title and picture#############
 #Custoum stylling
 col1, col2 = st.columns(2)
-st.markdown(""" <style> .font {
-height: 50px;
-font-family: 'Duru Sans';
-font-style: normal;
-font-weight: 400;
-font-size: 36px;
-letter-spacing: -0.02em;
-color: #824E1E;
-} </style> """, unsafe_allow_html=True)
-col1.markdown('<p class="font">🔎 Skinscancer 🔎</p>', unsafe_allow_html=True)
+col1.markdown(f"""<p style="color:#BB9301;font-size:30px"><strong>🔎 SKIN-SCANCER 🔎</strong></p>""", unsafe_allow_html=True)
 #col1 picture
 image1 = Image.open('assets/abcde.jpg')
 col1.image(image1, use_column_width=True)
-
-
 #column 2 picture
 image = Image.open('assets/Inspection_logo.jpg')
 col2.image(image, use_column_width=True)
-## trying to change the style of next subheader
-#st.markdown(""" <style> .font {
-#position: absolute;
-#height: 32px;
-#font-family: 'Inria Serif';
-#font-style: normal;
-#font-weight: 200;
-#font-size: 16px;
-#line-height: 160%;
-#color: #603D1E;
-#} </style> """, unsafe_allow_html=True)
-#st.markdown('<p class="font"> Check your skin using our website and have the **prediction**.</p>', unsafe_allow_html=True)
-st.subheader('Check your skin using our website and have the **prediction**.')
-
+st.markdown(f"""<p style="color:#BB9301;font-size:30px"><strong>Check your skin using our website and have the prediction.</strong></p>""", unsafe_allow_html=True)
 ################# DISCLAIMER ONLY CONTINUES IF THIS BOX IS CHECKED #################
 
 check1 = st.checkbox("I consent to the [Privacy Policy](https://google.com)")
 check2 = st.checkbox("I understand that the prediction can be wrong.")
 
-#if check1 and check2 == True:
-#    if st.button('Continue') == True:
-#    ########################Loading Image related#########
-st.subheader('Upload your image')
-uploaded_file = st.file_uploader(label='Pick an image to test')
-if uploaded_file is not None:
-    image_data = uploaded_file.getvalue()
-    st.image(image_data)
+if check1 and check2 == True:
+    if st.button('Continue') == True:
+    ########################Loading Image related#########
+        st.markdown(f"""<p style="color:#BB9301;font-size:30px"><strong>Upload your image</strong></p>""", unsafe_allow_html=True)
+        uploaded_file = st.file_uploader(label='Pick an image to test')
+        if uploaded_file is not None:
+            image_data = uploaded_file.getvalue()
+            st.image(image_data)
+            #############api#######################
+            skin_detection_api_url = 'https://skin-detection-hsuizqzdtq-ew.a.run.app/upload-image'
+            files = {'file': BytesIO(image_data)}
+            response = requests.post(skin_detection_api_url, files=files)
+            'Starting a long computation...'
+            # Add a placeholder
+            latest_iteration = st.empty()
+            bar = st.progress(0)
+            for i in range(150):
+            # Update the progress bar with each iteration.
+                latest_iteration.text(f'Trying to make a prediction {i+1}')
+                bar.progress(i + 1)
+                time.sleep(0.1)
+            '...and now we\'re done!'
+            prediction = response.json()
+            pred = prediction['possibility']
+            st.header(f'The probability of being malignant is ({pred})%')
 #########################Side bar################
 st.sidebar.header("This project was made by:")
 image_size = 50
@@ -129,20 +111,3 @@ CREATORS_HTML = f"""
 
 st.sidebar.write(CREATORS_HTML, unsafe_allow_html=True)
 ###############api#####################
-if uploaded_file is not None:
-    skin_detection_api_url = 'https://skin-detection-hsuizqzdtq-ew.a.run.app/upload-image'
-    files = {'file': BytesIO(image_data)}
-    response = requests.post(skin_detection_api_url, files=files)
-    'Starting a long computation...'
-    # Add a placeholder
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-    for i in range(100):
-    # Update the progress bar with each iteration.
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
-    '...and now we\'re done!'
-    prediction = response.json()
-    pred = prediction['possibility']
-    st.header(f'The probability of being malignant is round({pred})%')
